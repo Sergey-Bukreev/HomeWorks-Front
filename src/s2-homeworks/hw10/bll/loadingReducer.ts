590c72184c482a8adc612d2010649e0ab6d13cf3
BukreevSergey <bukreevwork2@gmail.com>
2024-03-13 19:10:55 +0200

    create loading-reducer, use it for change state of loading for loader. Add styles for loader

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,17 @@
-const initState = {
-    isLoading: false,
-}
+const initState:InitState10type = {isLoading: false,}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state:InitState10type = initState, action: Action10Type): InitState10type => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case "CHANGE_LOADING":
+            return {...state, isLoading:action.isLoading}
 
         default:
             return state
     }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
-
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+type LoadingActionType = { type: 'CHANGE_LOADING', isLoading: boolean }
+export type InitState10type = {isLoading: boolean}
+export type Action10Type = LoadingActionType
+export const loadingAC = (isLoading: boolean): LoadingActionType => ({type: 'CHANGE_LOADING', isLoading,})
