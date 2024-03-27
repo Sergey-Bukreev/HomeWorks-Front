64e043ea8a9d2549483dccbf2149c3bce599798a
BukreevSergey <bukreevwork2@gmail.com>
2024-03-27 13:55:18 +0200

    add types and logic for reducer for change theme. Add function for change theme id. Add styles

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
 const initState = {
     themeId: 1,
 }
+export type InitState12Type = {themeId:number}
+export type ChangeThemeIdActionType = {type:'SET_THEME_ID', id:number}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdActionType): InitState12Type => { // fix any
     switch (action.type) {
         // дописать
+        case "SET_THEME_ID":
+            return {...state, themeId:action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number):ChangeThemeIdActionType  => ({ type: 'SET_THEME_ID', id }) // fix any
